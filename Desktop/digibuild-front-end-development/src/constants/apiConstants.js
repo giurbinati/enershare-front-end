d849fed5dfa90958ef6ea425a16190a5c6037d36
giurbinati <127499485+giurbinati@users.noreply.github.com>
2024-06-26 19:24:46 +0200

    Delete Desktop/digibuild-front-end-development directory

--- Desktop/digibuild-front-end-development/src/constants/apiConstants.js
+++
@@ -1,4 +0,0 @@
-export const API_BASE_URL = 'http://147.213.76.216:4000';
-// export const ACCESS_TOKEN_NAME = 'login_access_token';
-// export const ROLES = 'roles';
-// export const USERNAME = 'username';
